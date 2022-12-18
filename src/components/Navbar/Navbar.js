93bd5898ff91a1d767b47725aee1f2f1893c5701
basemEk <basem.kreidly@gmail.com>
2022-12-18 12:55:32 +0200

    complete the responsive navbar navigations

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -45,7 +45,7 @@ function NavbarItem() {
           <a href="/offers-deals">OFFERS</a>
           <a href="/contact">CONTACT US</a>
           <a href="/subscribe" id="subscribe-nav">SUBSCRIBE</a>
-          <a href="#login" onClick={handleLogin}>
+          <a href="/login" onClick={handleLogin}>
             LOGIN
           </a>
         </div>
@@ -89,9 +89,9 @@ function NavbarItem() {
 
                 <Nav.Link href="/tournaments">TOURNAMENTS</Nav.Link>
                 <Nav.Link href="/offers-deals">OFFERS</Nav.Link>
-                <Nav.Link href="#contact">CONTACT US</Nav.Link>
-                <Nav.Link href="#subscribe" id='sub'>SUBSCRIBE</Nav.Link>
-                <Nav.Link href="#login" onClick={handleLogin}>LOGIN</Nav.Link>
+                <Nav.Link href="/contact">CONTACT US</Nav.Link>
+                <Nav.Link href="/subscribe" id='sub'>SUBSCRIBE</Nav.Link>
+                <Nav.Link href="/login" onClick={handleLogin}>LOGIN</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
